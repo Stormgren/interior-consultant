b35759e43a8f8ad74d858983f19617c29f497068
Stormgren <sbnv93@gmail.com>
2020-12-26 08:25:29 +0100

    Created page to match the design file

---
+++ scripts.js
@@ -0,0 +1,24 @@
+let menu = document.getElementById('menu');
+let container = document.getElementById('container');
+let btn = document.getElementById('hamburger');
+let btnb = document.getElementById('burger');
+let foota = document.getElementById('foota');
+let toggle = false;
+
+let toggable = () => {
+
+    toggle = !toggle;
+   
+if(toggle) {
+    menu.style.display = 'flex';
+    btn.innerHTML = '<img class="burger" src="images/close.svg" alt="Menu" />'
+  
+    } else { 
+        menu.style.display = 'none'; btn.innerHTML= '<img class="burger" src="images/menu.svg" alt="Menu" />'
+    }
+
+toggle ? container.style.display = 'none' : container.style.display = 'block';
+toggle ? foota.style.display = 'none' : foota.style.display = 'block';
+    console.log(toggle)
+
+}
